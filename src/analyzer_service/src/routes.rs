cf672dbc2807331d3657c9179c86c880588d6fd6
99rohernande <rodolfo.hernandez.external@sonova.com>
2026-09-16 10:38:31 +0100

    remove not used code

--- src/analyzer_service/src/routes.rs
+++
@@ -1,8 +0,0 @@
-use axum::{Router, routing::post};
-
-use crate::handlers::analyzer_service::analyze;
-
-pub fn create_routes() -> Router {
-    Router::new()
-        .route("/analyzer_service", post(analyze))
-}
